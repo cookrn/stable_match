c4e6e5f9f8a876ff4637a84524772331b9418adb
Ryan Cook <cookrn@gmail.com>
2012-05-01 15:39:14 -0600

    fix bug in `test_helper` that was breaking separate test suites

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ gem "minitest"
 require "minitest/autorun"
 require "minitest/pride"
 
-support_dir = File.expand_path File.dirname( __FILE__ ) , "support"
-Dir[ "#{ support_dir }/**/*.rb" ].each { | f | require f }
-
 require "stable_match"
+
+support_dir = File.join File.expand_path( File.dirname __FILE__ ) , "support"
+Dir[ "#{ support_dir }/**/*.rb" ].each { | f | require f }
